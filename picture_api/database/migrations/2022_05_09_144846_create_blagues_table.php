03d28699951769a17e096d6119c9eaee447edeff
Anthony <anthony.bellier@laposte.net>
2022-05-10 09:42:53 +0200

    correction site

--- picture_api/database/migrations/2022_05_09_144846_create_blagues_table.php
+++ picture_api/database/migrations/2022_05_09_144846_create_blagues_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('blagues', function (Blueprint $table) {
             $table->id();
-            $table->string('image',255);
+            $table->string('image',255)->default('https://fac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F12.2F07.2F4cff230b-512f-4b1d-abbb-90bf253fa9f2.2Ejpeg/345x258/quality/80/crop-from/center/chuck-norris.jpeg');
             $table->string('title',255);
             $table->text('text');
             $table->string('user',55);
